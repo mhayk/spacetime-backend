232ad9fa0be1b155492a2b193ba0bfbbe176743a
Mhayk Whandson <hi@mhayk.com>
2023-05-16 21:05:21 +0100

    feat: add and configure fastify

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+// HTTP Method: GET, POST, PUT, PATCH, DELETE
+
+app.post('/hello', () => {
+    return 'Hello World'
+})
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+})
